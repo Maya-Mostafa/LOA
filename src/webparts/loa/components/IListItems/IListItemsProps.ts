c50e0e44585dc18cd2d69e622d5fcc44a1808b1f
Mai Mostafa <mayamostafa@gmail.com>
2024-01-03 10:15:43 -0500

    adding context

--- src/webparts/loa/components/IListItems/IListItemsProps.ts
+++ src/webparts/loa/components/IListItems/IListItemsProps.ts
@@ -1,7 +1,10 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
 export interface IListItemsProps{
     items: any;
     filterField: any;
     preloaderVisible: boolean;    
     // schools: any;
     showEdit: boolean;
+    context: WebPartContext;
 }
